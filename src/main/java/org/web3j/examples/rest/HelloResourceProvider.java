df48089fef23cdfb2b09cba25f88679a2d9a3552
alexandrou <alexandrosrousos@gmail.com>
2020-01-30 16:39:46 +0000

    First commit.

---
+++ src/main/java/org/web3j/examples/rest/HelloResourceProvider.java
@@ -0,0 +1,39 @@
+package org.web3j.examples.rest;
+
+import org.keycloak.models.KeycloakSession;
+import org.keycloak.services.resource.RealmResourceProvider;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Produces;
+
+/**
+ * @author <a href="mailto:sthorger@redhat.com">Stian Thorgersen</a>
+ */
+public class HelloResourceProvider implements RealmResourceProvider {
+
+    private KeycloakSession session;
+
+    public HelloResourceProvider(KeycloakSession session) {
+        this.session = session;
+    }
+
+    @Override
+    public Object getResource() {
+        return this;
+    }
+
+    @GET
+    @Produces("text/plain; charset=utf-8")
+    public String get() {
+        String name = session.getContext().getRealm().getDisplayName();
+        if (name == null) {
+            name = session.getContext().getRealm().getName();
+        }
+        return "Hello " + name;
+    }
+
+    @Override
+    public void close() {
+    }
+
+}
---
+++ src/main/java/org/web3j/examples/rest/HelloResourceProviderFactory.java
@@ -0,0 +1,38 @@
+package org.web3j.examples.rest;
+
+import org.keycloak.Config.Scope;
+import org.keycloak.models.KeycloakSession;
+import org.keycloak.models.KeycloakSessionFactory;
+import org.keycloak.services.resource.RealmResourceProvider;
+import org.keycloak.services.resource.RealmResourceProviderFactory;
+
+/**
+ * @author <a href="mailto:sthorger@redhat.com">Stian Thorgersen</a>
+ */
+public class HelloResourceProviderFactory implements RealmResourceProviderFactory {
+
+    public static final String ID = "hello";
+
+    @Override
+    public String getId() {
+        return ID;
+    }
+
+    @Override
+    public RealmResourceProvider create(KeycloakSession session) {
+        return new HelloResourceProvider(session);
+    }
+
+    @Override
+    public void init(Scope config) {
+    }
+
+    @Override
+    public void postInit(KeycloakSessionFactory factory) {
+    }
+
+    @Override
+    public void close() {
+    }
+
+}
